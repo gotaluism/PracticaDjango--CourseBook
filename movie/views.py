89c0072291323b67a7ed233d38eecc6c603c0b7f
gotaluism <lgiraldo1109@gmail.com>
2023-02-13 08:09:06 -0500

    upload first time

---
+++ movie/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+def home(request):
+    searchTerm=request.GET.get('searchMovie')
+    return render(request, 'home.html',{'searchTerm':searchTerm})
+
+
+
+def about(request):
+    return HttpResponse('<h1>Welcome to about page</h1>')
+
+def signup(request):
+    email = request.GET.get('email')
+    return render(request,'signup.html',{'email':email})
