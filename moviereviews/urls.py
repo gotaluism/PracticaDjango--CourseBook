ad94ca087192fcf82ae97700d7928502e9c7d3fd
gotaluism <lgiraldo1109@gmail.com>
2023-02-28 19:53:45 -0500

    hastaCap10

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -18,15 +18,19 @@ from django.urls import path
 from movie import views as movieViews 
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import path,include
+
 
 from news import views as newsViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',movieViews.home,name='home'),
-    path('about',movieViews.about,name='about'),
-    path('signup',movieViews.signup, name='signup'),
-    path('news',newsViews.news,name='news'),
+    path('about/',movieViews.about,name='about'),
+    path('signup/',movieViews.signup, name='signup'),
+    path('news/',newsViews.news,name='news'),
+    path('movie/', include('movie.urls')),   #esto me asigna las nuevas ruta que ponga en app .url (movie) movie/urls.py
+    path('accounts/' ,include('accounts.urls')), 
 ]
 
-urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)   
